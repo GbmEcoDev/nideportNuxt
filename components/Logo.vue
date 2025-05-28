8cd0657d660410b5bdd4771e9569f7644833941f
arieloweb <arielorobles@gmail.com>
2025-05-28 10:49:44 -0300

    logo nideport svg

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,10 +1,10 @@
 <template>
      <div>
          <p v-if="color ? color === 'light' : $colorMode.value === 'light'">
-            <NuxtImg :src="`${urlImg}/images/nideport-by-gbm.svg`" alt="Nideport by GBM" class="py-0 w-[150px] md:w-[185px] lg:w-[185px]"/>
+            <NuxtImg :src="`${urlImg}/images/logo-nideport-dark.svg`" alt="Nideport" class="py-0 w-[150px] md:w-[185px] lg:w-[185px]"/>
              </p>
              <p v-if="color ? color === 'dark' : $colorMode.value === 'dark'">
-            <NuxtImg :src="`${urlImg}/images/nideport-by-gbm.svg`" alt="Nideport by GBM" class="w-[150px] md:w-[185px] lg:w-[185px]"/>
+            <NuxtImg :src="`${urlImg}/images/logo-nideport-dark.svg`" alt="Nideport" class="w-[150px] md:w-[185px] lg:w-[185px]"/>
              </p>
      </div>
 </template>
--- components/LogoFooter.vue
+++ components/LogoFooter.vue
@@ -1,10 +1,10 @@
 <template>
      <div>
          <p v-if="color ? color === 'light' : $colorMode.value === 'light'">
-            <NuxtImg :src="`${urlImg}/images/nideport-by-gbm-color.svg`" alt="Logo Nideport by GBM" class="py-0 w-[185px]" width="250"/>
+            <NuxtImg :src="`${urlImg}/images/logo-nideport.svg`" alt="Logo Nideport" class="py-0 w-[185px]" width="250"/>
              </p>
              <p v-if="color ? color === 'dark' : $colorMode.value === 'dark'">
-            <NuxtImg :src="`${urlImg}/images/nideport-by-gbm-color.svg`" alt="Logo Nideport by GBM" class="w-[185px]" width="250"/>
+            <NuxtImg :src="`${urlImg}/images/logo-nideport.svg`" alt="Logo Nideport" class="w-[185px]" width="250"/>
              </p>
      </div>
 </template>
